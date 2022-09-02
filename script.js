2a40dd2dd64ed6a07b7c56e4b8a272d282335027
I-Maged <magedhanafy@gmail.com>
2022-09-02 02:08:11 +0200

    first DOM

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+let sections = document.querySelectorAll('.section');
+let messageDisplay = document.querySelector('.message');
+let resultDisplay = document.querySelector('.result');
 let rockSpan = document.querySelector('.rock');
 let paperSpan = document.querySelector('.paper');
 let scissorSpan = document.querySelector('.scissor');
 
-rockSpan.addEventListener('click', function (e) {
-  randomPick();
-});
-paperSpan.addEventListener('click', function (e) {
-  randomPick();
-});
-scissorSpan.addEventListener('click', function (e) {
-  randomPick();
-});
-
 function randomPick() {
   let randomArray = ['rock', 'paper', 'scissor'];
 
-  console.log(randomArray[Math.floor(Math.random() * 3)]);
+  return randomArray[Math.floor(Math.random() * 3)];
+}
+
+function displayChoices(playerPick, pcPick) {
+  console.log(`You chose ${playerPick}. Computer chose ${pcPick}`);
+  messageDisplay.textContent = `You chose ${playerPick}. Computer chose ${pcPick}`;
+
+  if (playerPick == pcPick) {
+    resultDisplay.textContent = `Draw. You both chose ${pcPick}`;
+  } else {
+    resultDisplay.textContent = `Not draw`;
+  }
+}
+
+for (let i = 0; i < sections.length; i++) {
+  sections[i].addEventListener('click', function (e) {
+    let player = e.composedPath()[0].alt;
+    let pc = randomPick();
+
+    displayChoices(player, pc);
+  });
 }
